046e53ecb120cc570aad4e53433684a2b1a50c95
Mohit Soni <sp@decodeup.email>
2024-12-11 23:34:27 +0530

    Socket All Event Kept Properly

--- socket/socket.js
+++ socket/socket.js
@@ -1,135 +1,128 @@
 import { getDocThroughSocket } from "../controllers/doc.controller.js";
 import DocumentModel from "../models/documents.model.js";
 
+// In-memory room user management
 const roomUsers = {};
 
 const handleError = (error, callback) => {
   console.error(error);
-  callback('An error occurred. Please try again.');
+  if (callback) callback("An error occurred. Please try again.");
 };
 
 const addUserToRoom = (roomId, username, userId) => {
-  if (!roomUsers[roomId]) {
-    roomUsers[roomId] = [];
-  }
+  if (!roomUsers[roomId]) roomUsers[roomId] = [];
   roomUsers[roomId].push({ username, userId });
 };
 
-const removeUserFromRoom = (roomId, username, userId) => {
+const removeUserFromRoom = (roomId, userId) => {
   if (roomUsers[roomId]) {
     roomUsers[roomId] = roomUsers[roomId].filter(user => user.userId !== userId);
+    if (roomUsers[roomId].length === 0) delete roomUsers[roomId];
   }
 };
 
-const emitUserJoin = (io, roomId, username) => {
-  io.to(roomId).emit('someoneJoined', {
+const emitRoomUpdate = (io, roomId, event, username) => {
+  io.to(roomId).emit(event, {
     username,
-    roomUsers: roomUsers[roomId],
-  });
-};
-
-const emitUserLeave = (io, roomId, username) => {
-  io.to(roomId).emit('someoneLeft', {
-    username,
-    roomUsers: roomUsers[roomId],
+    roomUsers: roomUsers[roomId] || [],
   });
 };
 
 export const socket = (io) => {
-  io.on('connection', (socket) => {
+  io.on("connection", (socket) => {
     const userId = socket.id;
     console.log(`User connected: ${userId}`);
-    handleConnection(socket, io, userId);
+
+    socket.on("joinRoom", (data, callback) => handleJoinRoom(socket, io, data, userId, callback));
+    socket.on("leaveRoom", (data, callback) => handleLeaveRoom(socket, io, data, userId, callback));
+    socket.on("send-cursor", (data) => handleSendCursor(socket, data));
+    socket.on("send-changes", (data, callback) => handleSendChanges(io, data, callback));
+    socket.on("get-doc", (data) => handleGetDoc(io, data));
+    socket.on("save-doc", (data, callback) => handleSaveDoc(io,socket,data, callback));
+    socket.on("disconnect", () => handleDisconnect(socket, io, userId));
   });
 };
 
-const handleConnection = async (socket, io, userId) => {
-  socket.on('joinRoom', (data, callback) => {
-    try {
-      const { roomId, username } = data;
-      socket.join(roomId);
-      addUserToRoom(roomId, username, userId);
-      emitUserJoin(io, roomId, username);
-      callback(null);
-    } catch (error) {
-      handleError(error, callback);
-    }
-  });
+// Event Handlers
+const handleJoinRoom = (socket, io, data, userId, callback) => {
+  try {
+    const { roomId, username } = data;
+    socket.join(roomId);
+    addUserToRoom(roomId, username, userId);
+    emitRoomUpdate(io, roomId, "someoneJoined", username);
+  } catch (error) {
+    handleError(error, callback);
+  }
+};
 
-  socket.on('leaveRoom', (data, callback) => {
-    try {
-      const { roomId, username } = data;
-      socket.leave(roomId);
-      removeUserFromRoom(roomId, username, userId);
-      emitUserLeave(io, roomId, username);
-      callback(null);
-    } catch (error) {
-      handleError(error, callback);
-    }
-  });
+const handleLeaveRoom = (socket, io, data, userId, callback) => {
+  try {
+    const { roomId, username } = data;
+    socket.leave(roomId);
+    removeUserFromRoom(roomId, userId);
+    emitRoomUpdate(io, roomId, "someoneLeft", username);
+    callback(null);
+  } catch (error) {
+    handleError(error, callback);
+  }
+};
 
-  socket.on('send-cursor', (data) => {
-    try {
-      socket.to(data.roomId).emit('receive-cursor', {
-        username: data.username,
-        range: data.range
-      });
-    } catch (error) {
-      handleError(error);
-    }
-  });
+const handleSendCursor = (socket, data) => {
+  try {
+    socket.to(data.roomId).emit("receive-cursor", {
+      username: data.username,
+      range: data.range,
+    });
+  } catch (error) {
+    handleError(error);
+  }
+};
 
-  socket.on('send-changes', (data, callback) => {
-    try {
-      io.to(data.roomId).emit('receive-changes', {
-        delta: data.delta,
-        username: data.username
-      });
-      callback(null);
-    } catch (error) {
-      handleError(error, callback);
-    }
-  });
+const handleSendChanges = (io, data, callback) => {
+  try {
+    io.to(data.roomId).emit("receive-changes", {
+      delta: data.delta,
+      username: data.username,
+    });
+    callback(null);
+  } catch (error) {
+    handleError(error, callback);
+  }
+};
 
-  socket.on('get-doc', async (data) => {
-    try {
-      const doc = await getDocThroughSocket(data.docId);
-      const content = doc.content || '';
-      io.to(data.docId).emit('load-document', content);
-    } catch (error) {
-      handleError(error);
-    }
-  });
+const handleGetDoc = async (io, data) => {
+  try {
+    const doc = await getDocThroughSocket(data.docId);
+    const content = doc.content || "";
+    io.to(data.docId).emit("load-document", content);
+  } catch (error) {
+    handleError(error);
+  }
+};
 
-  socket.on('save-doc', async (data, callback) => {
-    try {
-      if (!data.data) return;
-      await DocumentModel.findByIdAndUpdate(data?.docId?.toString(), { content: data?.data });
-      callback(null);
-    } catch (error) {
-      handleError(error, callback);
-    }
-  });
+const handleSaveDoc = async (io,socket,data, callback) => {
+  try {
+    console.log('handleSaveDoc handleSaveDoc')
+    if (!data.data) return;
+    await DocumentModel.findByIdAndUpdate(data.docId?.toString(), { content: data.data });
+    console.log("'save-doc-receive 'save-doc-receive 'save-doc-receive",data.docId)
+    socket.to(data.docId.toString()).emit('save-doc-receive',{data:data.data})
+  } catch (error) {
+    handleError(error, callback);
+  }
+};
 
-  socket.on('disconnect', () => {
-    try {
-      let username, roomId;
-
-      Object.keys(roomUsers).forEach((currentRoomId) => {
-        const user = roomUsers[currentRoomId].find(user => user.userId === userId);
-        if (user) {
-          username = user.username;
-          roomId = currentRoomId;
-          removeUserFromRoom(currentRoomId, username, userId);
-        }
-      });
-
-      if (username && roomId) {
-        socket.leave(roomId);
-        emitUserLeave(io, roomId, username);
+const handleDisconnect = (socket, io, userId) => {
+  try {
+    Object.keys(roomUsers).forEach((roomId) => {
+      const user = roomUsers[roomId]?.find(user => user.userId === userId);
+      if (user) {
+        removeUserFromRoom(roomId, userId);
+        emitRoomUpdate(io, roomId, "someoneLeft", user.username);
       }
-    } catch (error) {
-      handleError(error);
-    }
-  });
+    });
+    console.log(`User disconnected: ${userId}`);
+  } catch (error) {
+    handleError(error);
+  }
 };
